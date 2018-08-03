391f8cfe7ba153d08f28b725473fcca5c723006d
Joanna Malukiewicz <jmalukie@gmail.com>
2018-08-03 12:32:31 -0400

    add r packages

--- Session9_DataViz/install_command.R
+++ Session9_DataViz/install_command.R
@@ -1,1 +1,4 @@
-install.packages(c("BSgenome.Hsapiens.UCSC.hg19","GenomicFeatures","GenomicRanges","ggdendro","ggplot2","gplots","grid","gridExtra","gtable","Gviz","RColorBrewer","reshape2","RMariaDB","rtracklayer","scales","tidyverse","TxDb.Hsapiens.UCSC.hg19.knownGene","vcfR","viridisLite"))
+install.packages(c("ggdendro", "ggplot2", "gplots", "grid", "gridExtra", "gtable", "RColorBrewer", "reshape2","RMariaDB", "scales", "tidyverse", "vcfR","viridisLite"))
+
+source("https://bioconductor.org/biocLite.R")
+biocLite(c("BSgenome.Hsapiens.UCSC.hg19","GenomicFeatures","GenomicRanges", "Gviz", "rtracklayer","TxDb.Hsapiens.UCSC.hg19.knownGene"))
